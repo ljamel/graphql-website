5bc997de3dd90c1fb27a1c982fd2a2ff7deb7f3c
root <root@DESKTOP-FIBPM3T.localdomain>
2021-05-06 19:23:30 +0200

    bug minor sqlachemy

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Configs
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +    os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Modules
 db = SQLAlchemy(app)
 
